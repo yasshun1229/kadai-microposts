fbc608e6c5559f3e6cf00cc1163cee4045f892ff
yasuda.shunsuke <yasshun1229.com>
2021-09-30 09:54:03 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @microposts = current_user.microposts.build # form_with 用
+      @micropost = current_user.microposts.build # form_with 用
       @pagy, @microposts = pagy(current_user.microposts.order(id: :desc))
     end
   end
