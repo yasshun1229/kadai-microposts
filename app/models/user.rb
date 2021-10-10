b165be398d3c00b87d0393357841abcd8a39942a
Ubuntu <ubuntu@ip-172-31-42-35.ap-northeast-1.compute.internal>
2021-10-10 05:38:33 +0000

    timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,4 +49,8 @@ class User < ApplicationRecord
   def liking?(other_micropost)
     self.likings.include?(other_micropost) # お気に入り登録しているMicropostを取得し、お気に入りではないMicropostが含まれていないか確認
   end
+  
+  # def feed_microposts
+  #   Micropost.where(user_id: self.likings_ids + [self.id])
+  # end
 end
